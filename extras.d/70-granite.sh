1fa9efbfedcb503c51d460bfa664b99b0d5c7227
Scott Moser <smoser@brickies.net>
2014-07-30 09:36:24 -0400

    Add support for flex hypervisor.
    nova-compute-flex provides a nova hypervisor driver for lxc.

---
+++ extras.d/70-granite.sh
@@ -0,0 +1,43 @@
+# granite.sh - Devstack extras script to install native lxc
+
+if [[ $VIRT_DRIVER == "granite" ]]; then
+   if [[ $1 == "source" ]]; then
+        # Keep track of the current directory
+        SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
+        TOP_DIR=$SCRIPT_DIR
+
+        echo $SCRIPT_DIR $TOP_DIR
+
+        # Import common functions
+        source $TOP_DIR/functions
+
+        # Load local configuration
+        source $TOP_DIR/stackrc
+
+        FILES=$TOP_DIR/files
+
+        # Get our defaults
+        source $TOP_DIR/lib/nova_plugins/hypervisor-granite
+        source $TOP_DIR/lib/granite
+    elif [[ $2 == "install" ]] ; then
+        echo_summary "Configuring granite"
+        if is_ubuntu; then
+            install_package python-software-properties
+            sudo apt-add-repository -y ppa:ubuntu-lxc/daily
+            apt_get update
+            install_package --force-yes lxc lxc-dev cgmanager
+            which cgm || install_package --force-yes cmanager-utils
+            name="$(id --name --user)"
+            fname="/etc/lxc/lxc-usernet"
+            for br in ${FLAT_NETWORK_BRIDGE} ${OVS_BRIDGE}; do
+               grep -q "^$name veth $br " $fname && continue
+               echo "$name veth $br 128"
+            done | sudo tee -a "$fname"
+            [ $? -eq 0 ] || echo "WARNING: failed to write to $fname"
+            sudo cgm create all $name
+            sudo cgm chown all $name $(id -u $name) $(id -g $name)
+            cgm movepid all $name $$
+        fi
+        install_granite
+    fi
+fi
