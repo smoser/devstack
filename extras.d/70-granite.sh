7689ac87d3e0dac8f2056c801cd84ba11b63fc94
Scott Moser <smoser@brickies.net>
2014-07-30 09:36:24 -0400

    Add support for granite as a nova hypervisor.
    Granite is a nova plugin allowing lxc nova hypervisor.

---
+++ extras.d/70-granite.sh
@@ -0,0 +1,32 @@
+# granite.sh - Devstack extras script to install native lxc
+
+if [[ $VIRT_DRIVER == "granite" ]]; then
+   if [[ $1 == "source" ]]; then
+        # Keep track of the current directory
+        SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
+        TOP_DIR=$SCRIPT_DIR
+
+        echo $SCRIPT_DIR $TOP_DIR
+
+        # Import common functions
+        source $TOP_DIR/functions
+
+        # Load local configuration
+        source $TOP_DIR/stackrc
+
+        FILES=$TOP_DIR/files
+
+        # Get our defaults
+        source $TOP_DIR/lib/nova_plugins/hypervisor-granite
+        source $TOP_DIR/lib/granite
+    elif [[ $2 == "install" ]] ; then
+        echo_summary "Configuring granite"
+        if is_ubuntu; then
+            install_package python-software-properties
+            sudo add-apt-repository -y ppa:ubuntu-lxc/daily
+            apt_get update
+            install_package --force-yes lxc lxc-dev
+        fi
+        install_granite
+    fi
+fi
